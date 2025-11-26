c01e6939a452db6cc4d1153a69a012b7f526ca46
Saksham Goyal <sakgoy2001@gmail.com>
2025-11-26 18:07:38 -0500

    bounds check

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,43 @@
 #![allow(nonstandard_style)]
+#![allow(unused)]
+#![allow(unreachable_code)]
 
 use windows_sys::{
 	Win32::{
+		Devices::HumanInterfaceDevice::{HID_USAGE_GENERIC_MOUSE, HID_USAGE_PAGE_GENERIC},
 		Foundation::*,
+		Graphics::Gdi::PtInRect,
 		System::LibraryLoader::GetModuleHandleW,
+		UI::Input::*,
 		UI::{Shell::*, WindowsAndMessaging::*},
-        UI::Input::*,
-        Graphics::Gdi::PtInRect,
 	},
 	core::*,
 };
 
 const WM_TRAYICON: u32 = WM_USER + 1;
 static mut NID: NOTIFYICONDATAW = unsafe { core::mem::zeroed() };
+static mut bIsListeningInput: bool = false;
 
 fn main() {
 	let class_name = w!("TRAYICONSCROLL");
 
-    let hInstance = unsafe { GetModuleHandleW(std::ptr::null()) };
+	let hInstance = unsafe { GetModuleHandleW(std::ptr::null()) };
 
-    let wndclass = WNDCLASSW {
-        lpfnWndProc: Some(WndProc),
-        hInstance: hInstance,
-        lpszClassName: class_name,
-        ..Default::default()
-    };
-
+	let wndclass = WNDCLASSW {
+		lpfnWndProc: Some(WndProc),
+		hInstance: hInstance,
+		lpszClassName: class_name,
+		..Default::default()
+	};
 
 	let result = unsafe { RegisterClassW(&wndclass) };
-    assert!(result != 0, "Failed to register window class: {:?}", unsafe { GetLastError() });
+	assert!(result != 0, "Failed to register window class: {:?}", unsafe { GetLastError() });
 
 	let hwnd: HWND = unsafe { CreateWindowExW(0, class_name, std::ptr::null_mut(), 0, 0, 0, 0, 0, std::ptr::null_mut(), std::ptr::null_mut(), hInstance, std::ptr::null_mut()) };
-    assert!(hwnd != std::ptr::null_mut(), "Failed to create window: {:?}", unsafe { GetLastError() });
+	assert!(hwnd != std::ptr::null_mut(), "Failed to create window: {:?}", unsafe { GetLastError() });
 
 	let icon_handle = unsafe { LoadIconW(std::ptr::null_mut(), IDI_APPLICATION) };
-    assert!(!icon_handle.is_null(), "Failed to load icon: {:?}", unsafe { GetLastError() });
+	assert!(!icon_handle.is_null(), "Failed to load icon: {:?}", unsafe { GetLastError() });
 
 	unsafe {
 		NID.cbSize = std::mem::size_of::<NOTIFYICONDATAW>() as u32;
@@ -77,13 +80,13 @@ fn convert_string_to_wide(s: &'static str) -> [u16; 128] {
 unsafe extern "system" fn WndProc(hWnd: HWND, message: u32, wParam: WPARAM, lParam: LPARAM) -> LRESULT {
 	CheckIfCursorIsInTrayIconBounds(hWnd);
 
-    return unsafe { DefWindowProcW(hWnd, message, wParam, lParam) };
+	return unsafe { DefWindowProcW(hWnd, message, wParam, lParam) };
 
 	// match message {
 	// 	WM_INPUT => {
 	// 		// Handle raw input for mouse wheel
-    //         let mut dwSize: u32 = std::mem::size_of::<RAWINPUT>() as u32;
-    //         let mut raw = RAWINPUT::default();
+	//         let mut dwSize: u32 = std::mem::size_of::<RAWINPUT>() as u32;
+	//         let mut raw = RAWINPUT::default();
 	// 	}
 	// 	WM_TRAYICON => {
 	// 		if lParam == WM_RBUTTONUP.try_into().unwrap() {
@@ -102,32 +105,48 @@ unsafe extern "system" fn WndProc(hWnd: HWND, message: u32, wParam: WPARAM, lPar
 }
 
 fn CheckIfCursorIsInTrayIconBounds(hWnd: HWND) -> bool {
-    return true;
-    // let niid = NOTIFYICONIDENTIFIER {
-    //     cbSize: std::mem::size_of::<NOTIFYICONIDENTIFIER>() as u32,
-    //     hWnd: unsafe { NID.hWnd },
-    //     uID: unsafe { NID.uID },
-    //     guidItem: unsafe { core::mem::zeroed() },
-    // };
+	let niid = NOTIFYICONIDENTIFIER {
+		cbSize: std::mem::size_of::<NOTIFYICONIDENTIFIER>() as u32,
+		hWnd: unsafe { NID.hWnd },
+		uID: unsafe { NID.uID },
+		guidItem: unsafe { core::mem::zeroed() },
+	};
 
-    // let mut ptCursor: POINT = POINT::default();
-    // let mut rcIcon: RECT = RECT::default();
-    // if unsafe { GetCursorPos(&mut ptCursor) } != 0 &&
-    //    unsafe { Shell_NotifyIconGetRect(&niid, &mut rcIcon) != 0 } &&
-    //    unsafe { PtInRect(&rcIcon, ptCursor) } != 0 {
-    //     print!("Cursor is within tray icon bounds.\n");
-    //     // Cursor is in tray icon bounds
-    //     // Register for raw input if not already registered
-    //     return true;
-    // } else {
-    //     // Cursor is outside tray icon bounds
-    //     // Unregister raw input if registered
-    //     print!("Cursor is outside tray icon bounds.\n");
-    //     return false;
-    // }
+	let mut ptCursor: POINT = POINT::default();
+	let mut rcIcon: RECT = RECT::default();
+	let curPos = unsafe { GetCursorPos(&mut ptCursor) } != 0;
+	let shell_rect_success = unsafe { Shell_NotifyIconGetRect(&niid, &mut rcIcon) == 0 };
+	let inRect = unsafe { PtInRect(&rcIcon, ptCursor) != 0 };
 
+	if curPos && shell_rect_success && inRect {
+		if (!unsafe { bIsListeningInput }) {
+			let rid = RAWINPUTDEVICE {
+				usUsagePage: HID_USAGE_PAGE_GENERIC,
+				usUsage: HID_USAGE_GENERIC_MOUSE,
+				dwFlags: RIDEV_INPUTSINK,
+				hwndTarget: hWnd,
+			};
+            let reg_result = unsafe { RegisterRawInputDevices(&rid, 1, std::mem::size_of::<RAWINPUTDEVICE>() as u32) };
+            if reg_result != 0 {
+                unsafe { bIsListeningInput = true; }
+            }
+		}
+		return true;
+	} else {
+		if (unsafe { bIsListeningInput }) {
+            let rid = RAWINPUTDEVICE {
+                usUsagePage: HID_USAGE_PAGE_GENERIC,
+                usUsage: HID_USAGE_GENERIC_MOUSE,
+                dwFlags: RIDEV_REMOVE,
+                hwndTarget: std::ptr::null_mut(),
+            };
+            unsafe { RegisterRawInputDevices(&rid, 1, std::mem::size_of::<RAWINPUTDEVICE>() as u32) };
+            unsafe { bIsListeningInput = false; }
+
+		}
+		return false;
+	}
 }
-
 
 unsafe fn ShowTrayMenu(hWnd: HWND) {
 	let mut pt: POINT = POINT::default();
